68634faf3af55024afc070ba93e5c72bd7928ef4
farleybsd <farley.t.i@hotmail.com>
2019-11-12 17:51:26 -0300

    Criando nossa primeira rota

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+app.listen(3000,()=>{
+    console.log('Estou Rodando na porta 3000')
+})
+
+app.get('/atendimentos',(req,res)=>{
+
+    res.send('Olá você está na rota de atendimento')
+
+})
