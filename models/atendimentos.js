05d2b99c0598484f82aced30039c8525410b3916
farleybsd <farley.t.i@hotmail.com>
2019-11-14 08:02:25 -0300

    Validacoes

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -4,23 +4,44 @@ const conexao = require('../infraestrutura/conexao')
 
 class Atendimento{
 
-    adiciona(atendimento,res){
-
+    adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
-        const data = moment(atendimento.data,'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentocomdata ={... atendimento,dataCriacao,data}
-        const sql= 'Insert Into atendimentos set ?'
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
 
-
-        conexao.query(sql,atendimentocomdata,(erro,resultados)=>{
-            if(erro){
-               res.status(400).json(erro)
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
-            else{
-                res.status(201).json(resultados)
-            }
+        ]
 
-        })
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(atendimento)
+                }
+            })
+        }
             
     }
 
